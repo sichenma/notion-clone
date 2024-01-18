4ca737a40d27358a59f8c578e80a15c37d8e7619
sichenma <beth.sma26@gmail.com>
2024-01-17 23:08:16 -0500

    init app with simple button ui

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+  //  <p className='text-red-500 font-bold'>
+  //   Hello Notion Clone
+    
+  //   </p>
+
+   <Button variant="outline">Button</Button>
+  )
+}
