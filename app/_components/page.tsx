572e4e917b7b373c480af52a51f3ec4f3a39e4ab
sichenma <beth.sma26@gmail.com>
2024-05-28 16:47:05 -0400

    learned routing in nextjs and added simple marketing page

---
+++ app/_components/page.tsx
@@ -0,0 +1,9 @@
+const componentsPage = () => {
+    return ( 
+        <div>
+            Components page. It shouldn't be a useful path. Because the folder name is leading with underscore.
+        </div> 
+    );
+}
+ 
+export default componentsPage;
---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+const testPage = () => {
+    return ( <div>
+        This is a test page. Go to localhost:3000/test. The path should work!
+    </div> );
+}
+ 
+export default testPage;
